bd8bfa408d8793bcb7d58b69532b4c54e93d573f
amanyay <Amanuelyayeh1@gmail.com>
2026-03-20 19:23:34 +0300

    March 20 Last url and db pass  commit

--- Backend/NodeWSql/db.js
+++ Backend/NodeWSql/db.js
@@ -4,7 +4,7 @@ async function createDBConnection() {
     const connection = await mysql.createConnection({
         host: process.env.DB_HOST,
         user: process.env.DB_USER,
-        password: '',
+        password: process.env.DB_PASSWORD,
         database: process.env.DB_NAME
     });
 
