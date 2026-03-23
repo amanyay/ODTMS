b83261628f2e51027b31b24ea4643632a32e1227
amanyay <Amanuelyayeh1@gmail.com>
2026-03-23 10:59:35 +0300

    March 23 Env file

--- Backend/NodeWSql/db.js
+++ Backend/NodeWSql/db.js
@@ -5,7 +5,7 @@ async function createDBConnection() {
     const connection = await mysql.createConnection({
         host: process.env.DB_HOST,
         user: process.env.DB_USER,
-        password: "",
+        password: process.env.DB_PASSWORD,
         database: process.env.DB_NAME
     });
 
